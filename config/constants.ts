c784b17a8b449f242b3da879b55a18f16ee422e8
Joewus12345 <joewus12345@gmail.com>
2025-08-14 14:49:32 +0000

    pisma database change and reduction of MAX_SESSION_MESSAGES from 20 to 10

--- config/constants.ts
+++ config/constants.ts
@@ -39,4 +39,4 @@ export const VECTOR_STORE_ID = "vs_688149954aa08191841d645c1b839941";
 export const DEFAULT_SEARCH_LIMIT = 10;
 
 // Maximum number of past session messages to return to the client
-export const MAX_SESSION_MESSAGES = 20;
+export const MAX_SESSION_MESSAGES = 10;
